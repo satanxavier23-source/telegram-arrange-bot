3929c97dd7351d94f0d77138848c50abd1c5c0ea
satanxavier23-source <satanxavier23@gmail.com>
2026-04-06 18:41:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, filters, ContextTypes
 import os
 import re
 
 TOKEN = os.getenv("TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Send photo or text with links 👀")
 
 async def clean_links(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = ""
@@ -13,18 +16,26 @@ async def clean_links(update: Update, context: ContextTypes.DEFAULT_TYPE):
     elif update.message.caption:
         text = update.message.caption
 
-    # find all links
-    links = re.findall(r'https?://\S+', text)
-
-    if not links:
+    if not text:
         return
 
-    message = "FULL VIDEO 👀🌸\n\n"
+    # find links
+    links = re.findall(r'https?://\S+', text)
 
-    for i, link in enumerate(links, 1):
-        message += f"VIDEO {i} ⤵️\n{link}\n\n"
+    # remove links from text
+    normal_text = re.sub(r'https?://\S+', '', text).strip()
 
-    # photo case
+    message = ""
+
+    if normal_text:
+        message += normal_text + "\n\n"
+
+    if links:
+        message += "FULL VIDEO 👀🌸\n\n"
+
+        for i, link in enumerate(links, 1):
+            message += f"VIDEO {i} ⤵️\n{link}\n\n"
+
     if update.message.photo:
         photo = update.message.photo[-1].file_id
         await update.message.reply_photo(photo=photo, caption=message)
@@ -35,6 +46,7 @@ async def clean_links(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 app = ApplicationBuilder().token(TOKEN).build()
 
+app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT | filters.PHOTO, clean_links))
 
 app.run_polling()
